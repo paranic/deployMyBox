ebeffbca8c1afd8f3b941074f2b89fa02ad6ad78
Parastatidis Nikos <paranic@gmail.com>
2020-06-14 02:30:53 +0300

    Add Sublime Text installation script

--- run.sh
+++ run.sh
@@ -34,6 +34,14 @@ apt-get update
 apt-get install -y code
 
 #
+# Install Sublime Text Editor
+#
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
+apt-get update
+apt-get install -y sublime-text
+
+#
 # Install Skype
 #
 wget https://repo.skype.com/latest/skypeforlinux-64.deb -P /tmp
